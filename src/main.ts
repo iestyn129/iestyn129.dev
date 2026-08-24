a9fc6ffd51950736be44ce015b3222612ddf28f4
iestyn129 <iestyn129msm@gmail.com>
2026-08-24 11:25:10 +0100

    init commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <div>
+    <h1>iestyn129.dev</h1>
+    <p>why are you here, go away</p>
+  </div>
+</section>
+`
