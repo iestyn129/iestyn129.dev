cb98cfc58850febdb990dbcb37a475912098a545
iestyn129 <iestyn129msm@gmail.com>
2026-08-24 11:37:10 +0100

    add link to source code

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>iestyn129.dev</h1>
     <p>why are you here, go away</p>
   </div>
+
+  <div>
+    <p>
+      the source code of this website is available here:<br>
+      <a href="https://github.com/iestyn129/iestyn129.dev" target="_blank" rel="noopener noreferrer">
+          https://github.com/iestyn129/iestyn129.dev
+      </a>
+    </p>
+  </div>
 </section>
 `
